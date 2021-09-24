d9fc2c165f3acb563ec2fd89ba5815ac7d91206b
jackaltsai <hata.s520@gmail.com>
2021-09-24 13:01:28 +0800

    SnapKit

--- SnapKit_Demo/ViewController.swift
+++ SnapKit_Demo/ViewController.swift
@@ -6,12 +6,47 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let childView = UIView()
+        childView.backgroundColor = .red
+        self.view.addSubview(childView)
+        
+        let anotherChildView = UIView()
+        anotherChildView.backgroundColor = .yellow
+        self.view.addSubview(anotherChildView)
+        
+        /*
+        1.size.equalTo
+        2.top.equalTo (Likewise, we have bottom.equalTo, left.equalTo and right.equalTo) with the offset.
+        3.centerX.equalTo and centerY.equalTo with the offset
+         */
+        
+        childView.snp.makeConstraints { (make) in
+          make.size.equalTo(CGSize(width: 300, height: 300))
+          make.top.equalTo(self.view.snp.top).offset(100)
+          make.centerX.equalTo(self.view)
+        }
+        
+        anotherChildView.snp.makeConstraints { (make) in
+          make.size.equalTo(childView)
+          make.top.equalTo(childView.snp.bottom).offset(50)
+          make.centerX.equalTo(self.view)
+        }
+        
+        
+        
+        
+        /* Demo_1 */
+//        childView.snp.makeConstraints { (make) in
+////            make.top.bottom.left.right.equalTo(self.view)
+//            make.edges.equalTo(self.view)
+//        }
     }
 
 
