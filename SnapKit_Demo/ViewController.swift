ec862807efa8f3b7bbb191bb0bdd6dc53119f3f7
jackaltsai <hata.s520@gmail.com>
2021-09-24 15:02:14 +0800

    The Responsive Stack

--- SnapKit_Demo/ViewController.swift
+++ SnapKit_Demo/ViewController.swift
@@ -40,23 +40,28 @@ class ViewController: UIViewController {
     private func layout() {
         view.addSubview(view1)
         view1.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.left.equalToSuperview()
+            make.height.equalTo(view1.snp.width)
+            make.centerY.equalToSuperview()
         }
         
         view.addSubview(view2)
         view2.snp.makeConstraints { make in
             
-            make.height.equalTo(40)
+            make.left.equalTo(view1.snp.right)
+            make.height.equalTo(view2.snp.width)
+            make.width.equalTo(view1)
             make.centerY.equalToSuperview()
-            make.leading.trailing.equalToSuperview()
         }
         
         view.addSubview(view3)
         view3.snp.makeConstraints { make in
             
-            make.width.equalTo(40)
-            make.centerX.equalToSuperview()
-            make.top.bottom.equalToSuperview()
+            make.left.equalTo(view2.snp.right)
+            make.height.equalTo(view3.snp.width)
+            make.right.equalToSuperview()
+            make.width.equalTo(view2)
+            make.centerY.equalToSuperview()
         }
         
     }
